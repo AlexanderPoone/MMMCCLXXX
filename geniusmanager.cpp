5301d669fef9d1bf41b5a709326cdf72480f85e8
Alexander-Claude Poon <alexander@link.cuhk.edu.hk>
2017-02-24 01:55:26 +0800

    　

--- geniusmanager.cpp
+++ geniusmanager.cpp
@@ -1,6 +1,6 @@
 #include "geniusmanager.h"
 
-const std::string base="http://api.genius.com";
+const std::string base="https://api.genius.com"; //All interaction with the API must be done over HTTPS.
 
 GeniusManager::GeniusManager()
 {
