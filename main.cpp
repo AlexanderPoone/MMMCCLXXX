001daf4724c7199355ce487cd77f411748660315
Alexander-Claude Poon <alexander@link.cuhk.edu.hk>
2017-03-06 22:12:45 +0800

    　

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QPixmap pixmap(":/popcornClouds.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
     MainWindow w;
     w.show();
 
